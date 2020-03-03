c03f69c9f5933a60afd7d41a0602742e48714932
martinnicolas <ttincho77@gmail.com>
2020-03-03 18:24:22 -0300

    Configuro locales y timezone

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,10 @@ module BaseRails5
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
+
+    #Configurar default locales y timezone
+    config.i18n.default_locale = :es
+    config.time_zone = 'America/Argentina/Buenos_Aires'
+ 	config.active_record.default_timezone = :local # Or :utc
   end
 end
